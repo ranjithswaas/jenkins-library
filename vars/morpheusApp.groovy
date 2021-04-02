4a86cf7b5b073f19a43b59c85d4d41afca0c5306
ranjithswaas <68428565+ranjithswaas@users.noreply.github.com>
2021-04-02 13:23:45 +0530

    Update morpheusApp.groovy

--- vars/morpheusApp.groovy
+++ vars/morpheusApp.groovy
@@ -1,5 +1,5 @@
 import groovy.json.JsonBuilder
-import org.tadamhicks.JenkinsHttpClient
+import org.ranjith.JenkinsHttpClient
 
 def buildApp(String morpheusUrl, Map<?, ?> postBody, String bearerToken) {
 	String jsoncontent = new JsonBuilder(postBody).toString()
